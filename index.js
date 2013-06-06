4a468ea13652c87ee75db7924fdb95931a9f3a28
anoopchaurasia <anoop26667@gmail.com>
2013-06-06 20:31:30 +0530

    Added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+require(__dirname+"/node_modules/jsfm.js");
+
+fm.Package("");
+fm.Class("Starter");
+Starter = function(){this.setMe=function(_me){me=_me;};
+	Static.main = function(){
+		var http = require('http');
+	};
+};
+
+
