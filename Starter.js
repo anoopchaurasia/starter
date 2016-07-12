f07ac1fa92f14d07754398a9a7a8887e5c695087
Anoop <anoop26667@gmail.com>
2016-07-12 18:37:21 +0530

    enhancement

--- Starter.js
+++ Starter.js
@@ -2,13 +2,13 @@ fm.Package("");
 fm.Class("Starter");
 Starter = function(me){this.setMe=function(_me){me=_me;};
 	'use strict';
-	var servletObj, expressApp, express;
+	var servletObj, express, expressApp;
 
 	this.init = function(){
-		Static.express = require('express');
-		Static.expressApp = me.express();
+		express = require('express');
+		expressApp = express();
 		var bodyParser = require('body-parser')
-		Static.expressApp.use( bodyParser.json({limit: '50mb'}));
+		expressApp.use( bodyParser.json({limit: '500mb'}));
 		Static.Const.preDefinedGetMethods = ["new", 'index', 'show'];
 		Static.Const.preDefinedPostMethods = ["create"];
 		Static.Const.preDefinedPatchMethods = ['edit', 'update'];
@@ -18,13 +18,13 @@ Starter = function(me){this.setMe=function(_me){me=_me;};
 	Static.createServer = function(app){
 		var host = app.host || 'localhost';
 		var port = app.port || 8080;
-		me.expressApp.listen(port, host, function(){
+		expressApp.listen(port, host, function(){
 			console.log("Server running at ", this._connectionKey);
 		});
 
-		me.expressApp.use("/jsfm", me.express.static(require("path").resolve(__dirname + '/' + 'node_modules/jsfm')));
+		expressApp.use("/jsfm", express.static(require("path").resolve(__dirname + '/' + 'node_modules/jsfm')));
 		//console.log(require("path").resolve(app.source));
-		app.source && me.expressApp.use(me.express.static(require("path").resolve(app.source)));
+		app.source && expressApp.use(express.static(require("path").resolve(app.source)));
 		handle(app);
 	};
 
@@ -42,12 +42,12 @@ Starter = function(me){this.setMe=function(_me){me=_me;};
 			applyMethods('head', [[], controllerconfig.head||[],  (instance.api && instance.api.head)|| []], instance, k);
 			if(controllerconfig['static']) {
 				controllerconfig['static'].forEach(function (path) {
-					me.expressApp.use(path.url, me.express.static(path.dir));
+					expressApp.use(path.url, express.static(path.dir));
 				});
 			}
 			if(controllerconfig["methods"]) {
 				controllerconfig['methods'].forEach(function (path) {
-					me.expressApp[path.type||'get'](path.url, instance[path.method || "index"]);
+					expressApp[path.type||'get'](path.url, instance[path.method || "index"]);
 				});
 			}
 		}
@@ -61,7 +61,7 @@ Starter = function(me){this.setMe=function(_me){me=_me;};
 		var usedMethods = {};
 		methods.forEach(function(method){
 			if(typeof instance[method] === 'function') {
-				me.expressApp[httpmethod]("/"+name + "/"+ method, instance[method]);
+				expressApp[httpmethod]("/"+name + "/"+ method, instance[method]);
 			}
 		});
 	};
