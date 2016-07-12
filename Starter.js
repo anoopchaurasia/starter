564fb63d90baae7aeeb0b0662c87bea62ff9ff09
Anoop <anoop26667@gmail.com>
2016-07-12 18:05:11 +0530

    enhancement

--- Starter.js
+++ Starter.js
@@ -5,10 +5,10 @@ Starter = function(me){this.setMe=function(_me){me=_me;};
 	var servletObj, expressApp, express;
 
 	this.init = function(){
-		express = require('express');
-		expressApp = express();
+		Static.express = require('express');
+		Static.expressApp = express();
 		var bodyParser = require('body-parser')
-		expressApp.use( bodyParser.json({limit: '50mb'}));
+		Static.expressApp.use( bodyParser.json({limit: '50mb'}));
 		Static.Const.preDefinedGetMethods = ["new", 'index', 'show'];
 		Static.Const.preDefinedPostMethods = ["create"];
 		Static.Const.preDefinedPatchMethods = ['edit', 'update'];
@@ -18,24 +18,18 @@ Starter = function(me){this.setMe=function(_me){me=_me;};
 	Static.createServer = function(app){
 		var host = app.host || 'localhost';
 		var port = app.port || 8080;
-		expressApp.listen(port, host, function(){
+		me.expressApp.listen(port, host, function(){
 			console.log("Server running at ", this._connectionKey);
 		});
 
-		expressApp.use("/jsfm", express.static(require("path").resolve(__dirname + '/' + 'node_modules/jsfm')));
+		me.expressApp.use("/jsfm", me.express.static(require("path").resolve(__dirname + '/' + 'node_modules/jsfm')));
 		//console.log(require("path").resolve(app.source));
-		app.source && expressApp.use(express.static(require("path").resolve(app.source)));
+		app.source && me.expressApp.use(me.express.static(require("path").resolve(app.source)));
 		handle(app);
 	};
 
 	function handle(app) {
 	 	var instance, controllerconfig;
-	 	var rooturl = app.rooturl;
-	 	if(rooturl) {
-	 		fm.Include(rooturl["class"]);
-			instance  = new (fm.stringToObject(rooturl["class"]))();
-			expressApp['get']("/", instance[rooturl.method || "index"]);
-	 	}
 		for(var k in app.controllers) {
 			controllerconfig = app.controllers[k];
 			fm.Include(controllerconfig["class"]);
@@ -48,7 +42,12 @@ Starter = function(me){this.setMe=function(_me){me=_me;};
 			applyMethods('head', [[], controllerconfig.head||[],  (instance.api && instance.api.head)|| []], instance, k);
 			if(controllerconfig['static']) {
 				controllerconfig['static'].forEach(function (path) {
-					expressApp.use(path.url, express.static(path.dir));
+					me.expressApp.use(path.url, me.express.static(path.dir));
+				});
+			}
+			if(controllerconfig["methods"]) {
+				controllerconfig['methods'].forEach(function (path) {
+					me.expressApp[path.type||'get'](path.url, instance[path.method || "index"]);
 				});
 			}
 		}
@@ -62,7 +61,7 @@ Starter = function(me){this.setMe=function(_me){me=_me;};
 		var usedMethods = {};
 		methods.forEach(function(method){
 			if(typeof instance[method] === 'function') {
-				expressApp[httpmethod]("/"+name + "/"+ method, instance[method]);
+				me.expressApp[httpmethod]("/"+name + "/"+ method, instance[method]);
 			}
 		});
 	};
